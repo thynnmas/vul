4c1d06cd65a32d9796972c52c47ce70d6a56594e
Tommy Schmid <ts.vul-dev@schmidx2.com>
2014-02-07 19:04:27 +0100

    Addes transofmration functions for SSE-vectors

--- Math/vul_aabb.hpp
+++ Math/vul_aabb.hpp
@@ -7,24 +7,23 @@
  *   the MIT licence applies (see the LICENCE file)
  */
 
-#ifndef VUL_AABB_H
-#define VUL_AABB_H
+#ifndef VUL_AABB_HPP
+#define VUL_AABB_HPP
 
 #include "vul_types.hpp"
+#include "vul_vector.hpp"
+#include "vul_matrix.hpp"
 
-// Define this for the c++11 version
+/**
+ * Define this for the c++11 version
+ */
 //#define VUL_CPLUSPLUS11
 
 namespace vul {
 	
 	//----------------
 	// Declarations
-	// 
-
-	template< typename T, i32_t n >
-	struct Vector;
-	template< typename T, i32_t n >
-	struct Affine;
+	// 	
 
 	template< typename T, i32_t n >
 	struct AABB {
@@ -41,12 +40,21 @@ namespace vul {
 		AABB< T, n > &operator=( const AABB< T, n > &rhs );
 	};
 
+	/**
+	 * Translate the AABB by a vector.
+	 */
 	template< typename T, i32_t n >
-	AABB< T, n > translate( const AABB< T, n > &aabb, const Vector< T, n > &vec );	// Translation
+	AABB< T, n > translate( const AABB< T, n > &aabb, const Vector< T, n > &vec );
+	/**
+	 * Scale the AABB (uniformly).
+	 */
 	template< typename T, i32_t n >
-	AABB< T, n > scale( const AABB< T, n > &aabb, T scalar );						// Scale
+	AABB< T, n > scale( const AABB< T, n > &aabb, T scalar );
+	/**
+	 * Perform an affine transformation on a single AABB.
+	 */
 	template< typename T, i32_t n >
-	AABB< T, n > transform( const AABB< T, n > &aabb, const Affine< T, n > &a );	// Transform
+	AABB< T, n > transform( const AABB< T, n > &aabb, const Affine< T, n > &a );
 	
 	/** 
 	 * Returns the center of an AABB
@@ -83,6 +91,32 @@ namespace vul {
 	template< typename T, i32_t n >
 	AABB< T, n > makeAABB( i32_t (& a)[ n * 2 ] );
 #endif
+
+	/**
+	 * Apply a 3D affine transform to multiple SSE packed AABBs with 3D-vectors.
+	 * Each AABB here contains 4 AABBs, where the vector data has the format __m128[ 3 ]:
+	 * __m128[ 0 ] = xxxx, __m128[ 1 ] = yyyy, __m128[ 2 ] = zzzz
+	 */
+	void transform3D( AABB< __m128, 3 > *out, const AABB< __m128, 3 > *in, const Affine< f32_t, 3 > &trans, ui32_t count );
+	/**
+	 * Apply a 3D affine transform to multiple SSE packed AABBs with 3D-vectors.
+	 * Each AABB here contains 2 AABBs, where the vector data has the format __m128d[ 3 ]:
+	 * __m128d[ 0 ] = xxxx, __m128d[ 1 ] = yyyy, __m128d[ 2 ] = zzzz
+	 */
+	void transform3D( AABB< __m128d, 3 > *out, const AABB< __m128d, 3 > *in, const Affine< f64_t, 3 > &trans, ui32_t count );
+	/**
+	 * Apply a 3D affine transform to multiple SSE packed AABBs with 3D-vectors.
+	 * Each AABB here contains 8 AABBs, where the vector data has the format __m256[ 3 ]:
+	 * __m256[ 0 ] = xxxx, __m256[ 1 ] = yyyy, __m256[ 2 ] = zzzz
+	 */
+	void transform3D( AABB< __m256, 3 > *out, const AABB< __m256, 3 > *in, const Affine< f32_t, 3 > &trans, ui32_t count );
+	/**
+	 * Apply a 3D affine transform to multiple SSE packed AABBs with 3D-vectors.
+	 * Each AABB here contains 4 AABBs, where the vector data has the format __m256d[ 3 ]:
+	 * __m256d[ 0 ] = xxxx, __m256d[ 1 ] = yyyy, __m256d[ 2 ] = zzzz
+	 */
+	void transform3D( AABB< __m256d, 3 > *out, const AABB< __m256d, 3 > *in, const Affine< f64_t, 3 > &trans, ui32_t count );
+	
 
 	//----------------
 	// Definition
@@ -210,26 +244,21 @@ namespace vul {
 		unsigned int i, j;
 
 #ifdef VUL_CPLUSPLUS11
-		newmin = Vector< T, n >( std::numeric_limits< T >::lowest( ) );
-		newmax = Vector< T, n >( std::numeric_limits< T >::max( ) );
+		newmin = Vector< T, n >( std::numeric_limits< T >::max( ) );
+		newmax = Vector< T, n >( std::numeric_limits< T >::lowest( ) );
 #else
-		newmin = makeVector< T, n >( std::numeric_limits< T >::lowest( ) );
-		newmax = makeVector< T, n >( std::numeric_limits< T >::max( ) );
+		newmin = makeVector< T, n >( std::numeric_limits< T >::max( ) );
+		newmax = makeVector< T, n >( std::numeric_limits< T >::lowest( ) );
 #endif
-		for( i = 0; i < 8; ++i )
+		for( i = 0; i < ( ui32_t )pow( 2.f, n ); ++i )
 		{
 			for( j = 0; j < n; ++j )
 			{
 				corner[ j ] = ( ( j & ( 1 << ( j - 1 ) ) ) != 0 ) ? aabb._max[ j ] : aabb._min[ j ];
 			}
 			corner = a * corner;
-#ifdef VUL_CPLUSPLUS11
-			newmin = Vector< T, n >( min( newmin, corner ) );
-			newmax = Vector< T, n >( max( newmax, corner ) );
-#else
-			newmin = makeVector< T, n >( min( newmin, corner ) );
-			newmax = makeVector< T, n >( max( newmax, corner ) );
-#endif
+			newmin = min( newmin, corner );
+			newmax = max( newmax, corner );
 		}
 #ifdef VUL_CPLUSPLUS11
 		res = AABB< T, n >( newmin, newmax );
@@ -255,6 +284,7 @@ namespace vul {
 	template< typename T, i32_t n >
 	bool inside( const AABB< T, n > &aabb, const Point< T, n > &pt )
 	{
+		// @TODO: Check everywhere if we might need to use fabs or is std::abs handles it. Possibly define our own templated abs that delegates to abs, fabs or our halfabs
 		return all(									// Are all coordinates'
 					abs( pt - center( aabb ) )		// distance to the center
 					<=								// smaller than or equal to
@@ -269,8 +299,264 @@ namespace vul {
 					abs( extent( aabb ) ) );		// the size of the extent
 	}
 
+	//--------------------------
+	// AOSOA SSE functions
+	// These are specializations for vectors of sse types (see vul_aosoa.hpp)
+	//
+	void transform3D( AABB< __m128, 3 > *out, const AABB< __m128, 3 > *in, const Affine< f32_t, 3 > &trans, ui32_t count )
+	{
+		ui32_t i, j, simdCount;
+		__m128 mat[ 4 ][ 3 ];
 
-	// @TODO: SSE version of transform ( AOSOA makes sense here )
+		// Fill the matrix from the Affine
+		for( i = 0; i < 3; ++i ) {
+			for( j = 0;j < 3; ++j ) {
+				mat[ i ][ j ] = _mm_set1_ps( trans.mat( i, j ) );
+			}
+		}
+		mat[ 3 ][ 0 ] = _mm_set1_ps( trans.vec[ 0 ] );
+		mat[ 3 ][ 1 ] = _mm_set1_ps( trans.vec[ 1 ] );
+		mat[ 3 ][ 2 ] = _mm_set1_ps( trans.vec[ 2 ] );
+
+		// Tranform
+		simdCount = ( count + 3 ) / 4;
+		for( i = 0; i < simdCount; ++i )
+		{
+			__m128 xNewMinis = {  FLT_MAX,  FLT_MAX,  FLT_MAX,  FLT_MAX };
+			__m128 xNewMaxes = { -FLT_MAX, -FLT_MAX, -FLT_MAX, -FLT_MAX };
+			__m128 yNewMinis = {  FLT_MAX,  FLT_MAX,  FLT_MAX,  FLT_MAX };
+			__m128 yNewMaxes = { -FLT_MAX, -FLT_MAX, -FLT_MAX, -FLT_MAX };
+			__m128 zNewMinis = {  FLT_MAX,  FLT_MAX,  FLT_MAX,  FLT_MAX };
+			__m128 zNewMaxes = { -FLT_MAX, -FLT_MAX, -FLT_MAX, -FLT_MAX };
+
+			for( j = 0; j < 8; ++j )
+			{
+				const AABB< __m128, 3 > bb = in[ i ];
+				__m128 xCorners = ( ( j & 1 ) != 0 ) ? bb._max[ 0 ] : bb._min[ 0 ];
+				__m128 yCorners = ( ( j & 2 ) != 0 ) ? bb._max[ 1 ] : bb._min[ 1 ];
+				__m128 zCorners = ( ( j & 4 ) != 0 ) ? bb._max[ 2 ] : bb._min[ 2 ];
+
+				__m128 xCornerTrans = _mm_add_ps( _mm_add_ps( _mm_mul_ps( xCorners, mat[ 0 ][ 0 ] ),
+															  _mm_mul_ps( yCorners, mat[ 1 ][ 0 ] ) ),
+												  _mm_add_ps( _mm_mul_ps( zCorners, mat[ 2 ][ 0 ] ),
+																					mat[ 3 ][ 0 ] ) );
+				__m128 yCornerTrans = _mm_add_ps( _mm_add_ps( _mm_mul_ps( xCorners, mat[ 0 ][ 1 ] ),
+															  _mm_mul_ps( yCorners, mat[ 1 ][ 1 ] ) ),
+												  _mm_add_ps( _mm_mul_ps( zCorners, mat[ 2 ][ 1 ] ),
+																					mat[ 3 ][ 1 ] ) );
+				__m128 zCornerTrans = _mm_add_ps( _mm_add_ps( _mm_mul_ps( xCorners, mat[ 0 ][ 2 ] ),
+															  _mm_mul_ps( yCorners, mat[ 1 ][ 2 ] ) ),
+												  _mm_add_ps( _mm_mul_ps( zCorners, mat[ 2 ][ 2 ] ),
+																					mat[ 3 ][ 2 ] ) );
+
+				xNewMinis = _mm_min_ps( xNewMinis, xCornerTrans );
+				xNewMaxes = _mm_max_ps( xNewMaxes, xCornerTrans );
+				yNewMinis = _mm_min_ps( yNewMinis, yCornerTrans );
+				yNewMaxes = _mm_max_ps( yNewMaxes, yCornerTrans );
+				zNewMinis = _mm_min_ps( zNewMinis, zCornerTrans );
+				zNewMaxes = _mm_max_ps( zNewMaxes, zCornerTrans );
+			}
+
+			AABB< __m128, 3 > *bb = &out[ i ];
+			bb->_min[ 0 ] = xNewMinis;
+			bb->_max[ 0 ] = xNewMaxes;
+			bb->_min[ 1 ] = yNewMinis;
+			bb->_max[ 1 ] = yNewMaxes;
+			bb->_min[ 2 ] = zNewMinis;
+			bb->_max[ 2 ] = zNewMaxes;
+		}
+	}
+	void transform3D( AABB< __m128d, 3 > *out, const AABB< __m128d, 3 > *in, const Affine< f64_t, 3 > &trans, ui32_t count )
+	{
+		ui32_t i, j, simdCount;
+		__m128d mat[ 4 ][ 3 ];
+
+		// Fill the matrix from the Affine
+		for( i = 0; i < 3; ++i ) {
+			for( j = 0;j < 3; ++j ) {
+				mat[ i ][ j ] = _mm_set1_pd( trans.mat( i, j ) );
+			}
+		}
+		mat[ 3 ][ 0 ] = _mm_set1_pd( trans.vec[ 0 ] );
+		mat[ 3 ][ 1 ] = _mm_set1_pd( trans.vec[ 1 ] );
+		mat[ 3 ][ 2 ] = _mm_set1_pd( trans.vec[ 2 ] );
+
+		// Tranform
+		simdCount = ( count + 1 ) / 2;
+		for( i = 0; i < simdCount; ++i )
+		{
+			__m128d xNewMinis = {  DBL_MAX,  DBL_MAX };
+			__m128d xNewMaxes = { -DBL_MAX, -DBL_MAX };
+			__m128d yNewMinis = {  DBL_MAX,  DBL_MAX };
+			__m128d yNewMaxes = { -DBL_MAX, -DBL_MAX };
+			__m128d zNewMinis = {  DBL_MAX,  DBL_MAX };
+			__m128d zNewMaxes = { -DBL_MAX, -DBL_MAX };
+
+			for( j = 0; j < 8; ++j )
+			{
+				const AABB< __m128d, 3 > bb = in[ i ];
+				__m128d xCorners = ( ( j & 1 ) != 0 ) ? bb._max[ 0 ] : bb._min[ 0 ];
+				__m128d yCorners = ( ( j & 2 ) != 0 ) ? bb._max[ 1 ] : bb._min[ 1 ];
+				__m128d zCorners = ( ( j & 4 ) != 0 ) ? bb._max[ 2 ] : bb._min[ 2 ];
+
+				
+				__m128d xCornerTrans = _mm_add_pd( _mm_add_pd( _mm_mul_pd( xCorners, mat[ 0 ][ 0 ] ),
+															   _mm_mul_pd( yCorners, mat[ 1 ][ 0 ] ) ),
+												   _mm_add_pd( _mm_mul_pd( zCorners, mat[ 2 ][ 0 ] ),
+																					 mat[ 3 ][ 0 ] ) );
+				__m128d yCornerTrans = _mm_add_pd( _mm_add_pd( _mm_mul_pd( xCorners, mat[ 0 ][ 1 ] ),
+															   _mm_mul_pd( yCorners, mat[ 1 ][ 1 ] ) ),
+												   _mm_add_pd( _mm_mul_pd( zCorners, mat[ 2 ][ 1 ] ),
+																					 mat[ 3 ][ 1 ] ) );
+				__m128d zCornerTrans = _mm_add_pd( _mm_add_pd( _mm_mul_pd( xCorners, mat[ 0 ][ 2 ] ),
+															   _mm_mul_pd( yCorners, mat[ 1 ][ 2 ] ) ),
+												   _mm_add_pd( _mm_mul_pd( zCorners, mat[ 2 ][ 2 ] ),
+												 									 mat[ 3 ][ 2 ] ) );
+				
+				xNewMinis = _mm_min_pd( xNewMinis, xCornerTrans );
+				xNewMaxes = _mm_max_pd( xNewMaxes, xCornerTrans );
+				yNewMinis = _mm_min_pd( yNewMinis, yCornerTrans );
+				yNewMaxes = _mm_max_pd( yNewMaxes, yCornerTrans );
+				zNewMinis = _mm_min_pd( zNewMinis, zCornerTrans );
+				zNewMaxes = _mm_max_pd( zNewMaxes, zCornerTrans );
+			}
+
+			AABB< __m128d, 3 > *bb = &out[ i ];
+			bb->_min[ 0 ] = xNewMinis;
+			bb->_max[ 0 ] = xNewMaxes;
+			bb->_min[ 1 ] = yNewMinis;
+			bb->_max[ 1 ] = yNewMaxes;
+			bb->_min[ 2 ] = zNewMinis;
+			bb->_max[ 2 ] = zNewMaxes;
+		}
+	}
+	void transform3D( AABB< __m256, 3 > *out, const AABB< __m256, 3 > *in, const Affine< f32_t, 3 > &trans, ui32_t count )
+	{
+		ui32_t i, j, simdCount;
+		__m256 mat[ 4 ][ 3 ];
+
+		// Fill the matrix from the Affine
+		for( i = 0; i < 3; ++i ) {
+			for( j = 0;j < 3; ++j ) {
+				mat[ i ][ j ] = _mm256_set1_ps( trans.mat( i, j ) );
+			}
+		}
+		mat[ 3 ][ 0 ] = _mm256_set1_ps( trans.vec[ 0 ] );
+		mat[ 3 ][ 1 ] = _mm256_set1_ps( trans.vec[ 1 ] );
+		mat[ 3 ][ 2 ] = _mm256_set1_ps( trans.vec[ 2 ] );
+
+		// Tranform
+		simdCount = ( count + 7 ) / 8;
+		for( i = 0; i < simdCount; ++i )
+		{
+			__m256 xNewMinis = {  FLT_MAX,  FLT_MAX,  FLT_MAX,  FLT_MAX };
+			__m256 xNewMaxes = { -FLT_MAX, -FLT_MAX, -FLT_MAX, -FLT_MAX };
+			__m256 yNewMinis = {  FLT_MAX,  FLT_MAX,  FLT_MAX,  FLT_MAX };
+			__m256 yNewMaxes = { -FLT_MAX, -FLT_MAX, -FLT_MAX, -FLT_MAX };
+			__m256 zNewMinis = {  FLT_MAX,  FLT_MAX,  FLT_MAX,  FLT_MAX };
+			__m256 zNewMaxes = { -FLT_MAX, -FLT_MAX, -FLT_MAX, -FLT_MAX };
+
+			for( j = 0; j < 8; ++j )
+			{
+				const AABB< __m256, 3 > bb = in[ i ];
+				__m256 xCorners = ( ( j & 1 ) != 0 ) ? bb._max[ 0 ] : bb._min[ 0 ];
+				__m256 yCorners = ( ( j & 2 ) != 0 ) ? bb._max[ 1 ] : bb._min[ 1 ];
+				__m256 zCorners = ( ( j & 4 ) != 0 ) ? bb._max[ 2 ] : bb._min[ 2 ];
+
+				
+				__m256 xCornerTrans = _mm256_add_ps( _mm256_add_ps( _mm256_mul_ps( xCorners, mat[ 0 ][ 0 ] ),
+																	_mm256_mul_ps( yCorners, mat[ 1 ][ 0 ] ) ),
+												     _mm256_add_ps( _mm256_mul_ps( zCorners, mat[ 2 ][ 0 ] ),
+																							 mat[ 3 ][ 0 ] ) );
+				__m256 yCornerTrans = _mm256_add_ps( _mm256_add_ps( _mm256_mul_ps( xCorners, mat[ 0 ][ 1 ] ),
+																	_mm256_mul_ps( yCorners, mat[ 1 ][ 1 ] ) ),
+													 _mm256_add_ps( _mm256_mul_ps( zCorners, mat[ 2 ][ 1 ] ),
+																							 mat[ 3 ][ 1 ] ) );
+				__m256 zCornerTrans = _mm256_add_ps( _mm256_add_ps( _mm256_mul_ps( xCorners, mat[ 0 ][ 2 ] ),
+																	_mm256_mul_ps( yCorners, mat[ 1 ][ 2 ] ) ),
+													 _mm256_add_ps( _mm256_mul_ps( zCorners, mat[ 2 ][ 2 ] ),
+																							 mat[ 3 ][ 2 ] ) );
+
+				xNewMinis = _mm256_min_ps( xNewMinis, xCornerTrans );
+				xNewMaxes = _mm256_max_ps( xNewMaxes, xCornerTrans );
+				yNewMinis = _mm256_min_ps( yNewMinis, yCornerTrans );
+				yNewMaxes = _mm256_max_ps( yNewMaxes, yCornerTrans );
+				zNewMinis = _mm256_min_ps( zNewMinis, zCornerTrans );
+				zNewMaxes = _mm256_max_ps( zNewMaxes, zCornerTrans );
+			}
+
+			AABB< __m256, 3 > *bb = &out[ i ];
+			bb->_min[ 0 ] = xNewMinis;
+			bb->_max[ 0 ] = xNewMaxes;
+			bb->_min[ 1 ] = yNewMinis;
+			bb->_max[ 1 ] = yNewMaxes;
+			bb->_min[ 2 ] = zNewMinis;
+			bb->_max[ 2 ] = zNewMaxes;
+		}
+	}
+	void transform3D( AABB< __m256d, 3 > *out, const AABB< __m256d, 3 > *in, const Affine< f64_t, 3 > &trans, ui32_t count )
+	{
+		ui32_t i, j, simdCount;
+		__m256d mat[ 4 ][ 3 ];
+
+		// Fill the matrix from the Affine
+		for( i = 0; i < 3; ++i ) {
+			for( j = 0;j < 3; ++j ) {
+				mat[ i ][ j ] = _mm256_set1_pd( trans.mat( i, j ) );
+			}
+		}
+		mat[ 3 ][ 0 ] = _mm256_set1_pd( trans.vec[ 0 ] );
+		mat[ 3 ][ 1 ] = _mm256_set1_pd( trans.vec[ 1 ] );
+		mat[ 3 ][ 2 ] = _mm256_set1_pd( trans.vec[ 2 ] );
+
+		// Tranform
+		simdCount = ( count + 3) / 4;
+		for( i = 0; i < simdCount; ++i )
+		{
+			__m256d xNewMinis = {  DBL_MAX,  DBL_MAX };
+			__m256d xNewMaxes = { -DBL_MAX, -DBL_MAX };
+			__m256d yNewMinis = {  DBL_MAX,  DBL_MAX };
+			__m256d yNewMaxes = { -DBL_MAX, -DBL_MAX };
+			__m256d zNewMinis = {  DBL_MAX,  DBL_MAX };
+			__m256d zNewMaxes = { -DBL_MAX, -DBL_MAX };
+
+			for( j = 0; j < 8; ++j )
+			{
+				const AABB< __m256d, 3 > bb = in[ i ];
+				__m256d xCorners = ( ( j & 1 ) != 0 ) ? bb._max[ 0 ] : bb._min[ 0 ];
+				__m256d yCorners = ( ( j & 2 ) != 0 ) ? bb._max[ 1 ] : bb._min[ 1 ];
+				__m256d zCorners = ( ( j & 4 ) != 0 ) ? bb._max[ 2 ] : bb._min[ 2 ];
+
+				__m256d xCornerTrans = _mm256_add_pd( _mm256_add_pd( _mm256_mul_pd( xCorners, mat[ 0 ][ 0 ] ),
+																	 _mm256_mul_pd( yCorners, mat[ 1 ][ 0 ] ) ),
+												      _mm256_add_pd( _mm256_mul_pd( zCorners, mat[ 2 ][ 0 ] ),
+																							  mat[ 3 ][ 0 ] ) );
+				__m256d yCornerTrans = _mm256_add_pd( _mm256_add_pd( _mm256_mul_pd( xCorners, mat[ 0 ][ 1 ] ),
+																	 _mm256_mul_pd( yCorners, mat[ 1 ][ 1 ] ) ),
+													  _mm256_add_pd( _mm256_mul_pd( zCorners, mat[ 2 ][ 1 ] ),
+																							  mat[ 3 ][ 1 ] ) );
+				__m256d zCornerTrans = _mm256_add_pd( _mm256_add_pd( _mm256_mul_pd( xCorners, mat[ 0 ][ 2 ] ),
+																	 _mm256_mul_pd( yCorners, mat[ 1 ][ 2 ] ) ),
+													  _mm256_add_pd( _mm256_mul_pd( zCorners, mat[ 2 ][ 2 ] ),
+																							  mat[ 3 ][ 2 ] ) );
+
+				xNewMinis = _mm256_min_pd( xNewMinis, xCornerTrans );
+				xNewMaxes = _mm256_max_pd( xNewMaxes, xCornerTrans );
+				yNewMinis = _mm256_min_pd( yNewMinis, yCornerTrans );
+				yNewMaxes = _mm256_max_pd( yNewMaxes, yCornerTrans );
+				zNewMinis = _mm256_min_pd( zNewMinis, zCornerTrans );
+				zNewMaxes = _mm256_max_pd( zNewMaxes, zCornerTrans );
+			}
+
+			AABB< __m256d, 3 > *bb = &out[ i ];
+			bb->_min[ 0 ] = xNewMinis;
+			bb->_max[ 0 ] = xNewMaxes;
+			bb->_min[ 1 ] = yNewMinis;
+			bb->_max[ 1 ] = yNewMaxes;
+			bb->_min[ 2 ] = zNewMinis;
+			bb->_max[ 2 ] = zNewMaxes;
+		}
+	}
 }
 
 #endif

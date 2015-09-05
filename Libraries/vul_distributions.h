8f8e753223f800c14004c0391f640384712e6368
Tommy Schmid <ts.vul-dev@schmidx2.com>
2015-09-06 01:01:05 +0200

    Moved halton pair to halton series; just make multiple for multiple dimensions

--- Libraries/vul_distributions.h
+++ Libraries/vul_distributions.h
@@ -2,7 +2,7 @@
  * Villains' Utility Library - Thomas Martin Schmid, 2015. Public domain?
  *
  * This file contains several number distribution functions.
- * -vul_halton_par: Halton series of 2 given numbers. Distributes values 
+ * -vul_halton_t: Halton series of a given base number. Distributes values 
  *					uniformely in the range [0, 1]
  * 
  * ? If public domain is not legally valid in your legal jurisdiction
@@ -26,20 +26,20 @@
  * State of a vul_halton_parir
  */
 typedef struct {
-	f32_t value_a, value_b;
-	f32_t inv_base_a, inv_base_b;
-} vul_halton_pair_t;
+	f32_t value;
+	f32_t inv_base;
+} vul_halton_t;
 
 /**
- * Create a vul_halton_pair state.  Uses Thomas Wang's integer hash to 
-* initialize values from the seed.
-*/
+ * Create a vul_halton_t state.  Uses Thomas Wang's integer hash to 
+ * initialize values from the seed.
+ */
 #ifndef VUL_DEFINE
-vul_halton_pair_t *vul_halton_pair_create( ui32_t base1, ui32_t base2, ui32_t seed );
+vul_halton_t *vul_halton_series_create( ui32_t base1, ui32_t seed );
 #else
-vul_halton_pair_t *vul_halton_pair_create( ui32_t base1, ui32_t base2, ui32_t seed )
+vul_halton_t *vul_halton_series_create( ui32_t base1, ui32_t seed )
 {
-	vul_halton_pair_t *r = ( vul_halton_pair_t* )malloc( sizeof( vul_halton_pair_t ) );
+	vul_halton_t *r = ( vul_halton_t* )malloc( sizeof( vul_halton_t ) );
 	assert( r != NULL );
 
 	seed = (seed ^ 61) ^ (seed >> 16);
@@ -47,15 +47,9 @@ vul_halton_pair_t *vul_halton_pair_create( ui32_t base1, ui32_t base2, ui32_t se
 	seed = seed ^ (seed >> 4);
 	seed *= 0x27d4eb2d;
 	seed = seed ^ (seed >> 15);
-	r->value_a = ( f32_t )seed / 4294967295.f;
+	r->value = ( f32_t )seed / 4294967295.f;
 
-	seed ^= ( seed << 13 );
-	seed ^= ( seed >> 17 );
-	seed ^= ( seed << 5 );
-
-	r->value_b = ( f32_t )seed / 4294967295.f;
-	r->inv_base_a = 1.f / base1;
-	r->inv_base_b = 1.f / base2;
+	r->inv_base = 1.f / base1;
 
 	return r;
 }
@@ -65,9 +59,9 @@ vul_halton_pair_t *vul_halton_pair_create( ui32_t base1, ui32_t base2, ui32_t se
  * Destroy a vul_halton_pair state.
  */
 #ifndef VUL_DEFINE
-void vul_halton_pair_destroy( vul_halton_pair_t *r );
+void vul_halton_series_destroy( vul_halton_t *r );
 #else
-void vul_halton_pair_destroy( vul_halton_pair_t *r )
+void vul_halton_series_destroy( vul_halton_t *r )
 {
 	assert( r != NULL );
 	free( r );
@@ -75,50 +69,28 @@ void vul_halton_pair_destroy( vul_halton_pair_t *r )
 #endif
 
 /**
- * Advances the state of the halton pair values.
+ * Advances the state of the halton series and return 
+ * the next value.
  */
 #ifndef VUL_DEFINE
-void vul_halton_pair_next( vul_halton_pair_t *rng );
+f32_t vul_halton_series_next( vul_halton_t *rng );
 #else
-void vul_halton_pair_next( vul_halton_pair_t *rng )
+f32_t vul_halton_series_next( vul_halton_t *rng )
 {
 	assert( rng != NULL );
 	
-	f32_t r = 1.f - rng->value_a - 0.0000001f;
-	if ( rng->inv_base_a < r ) {
-		rng->value_a += rng->inv_base_a;
+	f32_t r = 1.f - rng->value - 0.0000001f;
+	if ( rng->inv_base < r ) {
+		rng->value += rng->inv_base;
 	} else {
-		f32_t h = rng->inv_base_a, h2;
+		f32_t h = rng->inv_base, h2;
 		do{
 			h2 = h; 
-			h *= rng->inv_base_a;
+			h *= rng->inv_base;
 		} while ( h >= r );
-		rng->value_a += h2 + h - 1.f;
+		rng->value += h2 + h - 1.f;
 	}
-	r = 1.f - rng->value_b - 0.0000001f;
-	if ( rng->inv_base_b < r ) {
-		rng->value_b += rng->inv_base_b;
-	} else {
-		f32_t h = rng->inv_base_b, h2;
-		do{
-			h2 = h; 
-			h *= rng->inv_base_b;
-		} while ( h >= r );
-		rng->value_b += h2 + h - 1.f;
-	}
-}
-#endif
-
-/**
- * Fetch the current values of the halton pair.
- */
-#ifndef VUL_DEFINE
-void vul_halton_pair_fetch( vul_halton_pair_t *rng, f32_t *v1, f32_t *v2 );
-#else
-void vul_halton_pair_fetch( vul_halton_pair_t *rng, f32_t *v1, f32_t *v2 )
-{
-	*v1 = rng->value_a;
-	*v2 = rng->value_b;
+	return rng->value;
 }
 #endif
 
